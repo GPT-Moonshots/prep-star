e02803749054616a57a973b03076c7c1c82ce5d7
Pudv95 <vivekupadhayay18@gmail.com>
2024-06-20 16:01:55 +0530

    take image from camera or gallery

--- lib/View/Practice/practice.dart
+++ lib/View/Practice/practice.dart
@@ -1,10 +1,17 @@
-import 'package:flutter/material.dart';
-import 'package:prepstar/Service/Auth/o_auth.dart';
-import 'package:prepstar/Service/Database/course.dart';
+import 'dart:io';
 
-class Practice extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:prepstar/View/Practice/Camera/camera_whatsapp.dart';
+
+class Practice extends StatefulWidget {
   const Practice({super.key});
 
+  @override
+  State<Practice> createState() => _PracticeState();
+}
+
+class _PracticeState extends State<Practice> {
+  final files = ValueNotifier(<File>[]);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,21 +22,37 @@ class Practice extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-                onPressed: () async {
-                  OauthService.logout(context);
+            const Card(
+              child: Text('Chat with AI'),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.5,
+              child: ListView.builder(
+                itemCount: files.value.length,
+                itemBuilder: (context, index) {
+                  return SizedBox(
+                    height: 200,
+                    width: 200,
+                    child: Image.file(files.value[index]),
+                  );
                 },
-                child: Text('Logout')),
-            ElevatedButton(
-              onPressed: () async {
-                await CourseDatabase.uploadCourse().then((value) {
-                  print('Course uploaded successfully!');
-                });
-              },
-              child: Text('Upload Course'),
-            )
+              ),
+            ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.camera_alt),
+        onPressed: () async {
+          List<File>? res = await Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const WhatsappCamera(),
+            ),
+          );
+          setState(() {});
+          if (res != null) files.value = res;
+        },
       ),
     );
   }
