bf8d9cc74951b886f68e02269a59899729cf4f25
Pudv95 <vivekupadhayay18@gmail.com>
2024-06-23 18:51:33 +0530

    add chat page

--- lib/Controller/controller.dart
+++ lib/Controller/controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class AppController {
@@ -6,5 +7,9 @@ class AppController {
     FlutterSecureStorage storage = const FlutterSecureStorage();
     String? uid = await storage.read(key: 'uid');
     return uid!;
+  }
+
+  static String getGPTkeyFromEnv() {
+    return dotenv.get('OPENAI_API_KEY');
   }
 }
